d38b2001d2cb7dd3b9897d2211229d7f5e752163
Breno Stacheski <brenosttc@gmail.com>
2025-01-16 22:39:17 -0300

    config: removing SSR configuration and defining environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+
+  production: true,
+  API_URL: 'http://rest-items.research.cloudonix.io/',
+  CLOUDONIX_VERSION: '0.0.0',
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+
+  production: false,
+  API_URL: 'http://rest-items.research.cloudonix.io/',
+  CLOUDONIX_VERSION: '0.0.0',
+}
