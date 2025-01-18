96a3a455d6cb363c2fd9a6fffaea6f2232dca31b
Breno Stacheski <brenosttc@gmail.com>
2025-01-18 15:26:41 -0300

    feat: adding interceptor to app module provider and lazy loading products module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { AppComponent } from "./app.component";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HttpClientModule } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { InlineSVGModule } from 'ng-inline-svg-2';
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { SharedModule } from "./features/nav/shared.module";
+import { AuthInterceptor } from "./core/interceptors/auth.interceptor";
+import { NgxMaskModule } from "ngx-mask";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
@@ -20,13 +23,20 @@ import { RouterModule } from "@angular/router";
     HttpClientModule,
     AppRoutingModule,
     InlineSVGModule.forRoot(),
+    NgxMaskModule.forRoot(),
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    SharedModule
   ],
   providers: [
     provideAnimationsAsync(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
